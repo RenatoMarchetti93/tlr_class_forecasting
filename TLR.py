fbf8e5dc7cb2b6e4507b71801192d3073f983de1
MIPU_energy <renato.marchetti@mipu.eu>
2024-02-09 10:49:20 +0100

    first conceptual stable

--- TLR.py
+++ TLR.py
@@ -22,7 +22,7 @@ class Preprocessing():
 
     def __init__(self,df: pd.DataFrame, dict_features: dict):
         self.df = df
-        self.dict_features = dict_features
+        self.dict_features =  dict_features
         self.time_shifts = [] # numero di ore indietro
 
     @staticmethod
@@ -138,7 +138,7 @@ class Preprocessing():
             """
             return self.time_shifts
 
-    def preprocess(self, target: str) -> pd.DataFrame:
+    def preprocess_tlr(self, target: str, is_train = True) -> pd.DataFrame:
 
         """
         Esegue l'intero processo di preprocessing sul DataFrame.
@@ -150,9 +150,21 @@ class Preprocessing():
         df = self.add_rolling_stats(df, target)
         df = self.add_seasonal_stats(df, target)
 
-        df = df.dropna()
+        if is_train == True:
+            df = df.dropna()
+
         self.df = df
         return df
+
+    def prepocess_coge(self):
+        """
+        Esegue la creazione di lag sul DataFrame.
+        :return: DataFrame preprocessato.
+
+        """
+        df = self.add_lagged_features(df)
+
+
 
     def split_data_temporally(self, df: pd.DataFrame, target_column: str, split_date: str = None, train_ratio: float = None):
         ################################# QUI #############################
@@ -186,6 +198,7 @@ class Preprocessing():
 
     def fit_grid(self, models_params: list, X_train: pd.DataFrame, y_train: pd.Series, cv=5):
         
+
         # creare attributo model params in modo da non passarlo nel fit_grid
 
         """
@@ -240,11 +253,15 @@ class Preprocessing():
         return {'MSE': mse, 'MAE': mae, 'R2': r2}
 
     # def pre_predict()
-    def predict(self, df: pd.DataFrame, start:str, horizon_window: int) -> pd.Series:
+    def predict(self, df: pd.DataFrame, start:str, horizon_window: int, df_is_preprocessed = False) -> pd.Series:
         
+        # if not df_is_preprocessed:
+        #     inner_prep = Preprocessing(df,self.dict_features)
+        #     df = inner_prep.preprocess_tlr(self.target)
         y_pred = []
-        max_hour_back = self.time_shifts
-        ind = df.loc[pd.to_datetime(start)-pd.Timedelta(hours = max_hour_back):pd.to_datetime(start) + pd.Timedelta(hours = horizon_window)].index
+        max_hour_back = max(self.time_shifts)
+        # ind = df.loc[pd.to_datetime(start)-pd.Timedelta(hours = max_hour_back):pd.to_datetime(start) + pd.Timedelta(hours = horizon_window)].index
+        ind = df.loc[pd.to_datetime(start):pd.to_datetime(start) + pd.Timedelta(hours = horizon_window)].index
         # da tradurre in indici numerici e non temporali
         for i in ind:
 
@@ -261,13 +278,13 @@ class Preprocessing():
                 if var != self.target:
 
                     rolling_window = df[var].rolling(f'{h_window}H', min_periods=h_window)
-                    df.loc[i,f'median_rolling_{var}'] = rolling_window.median()
-                    df.loc[i,f'dev_std_rolling_{var}'] = rolling_window.std()
-                    df.loc[i,f'mean_rolling_{var}'] = rolling_window.mean()
-                    df.loc[i,f'max_rolling_{var}'] = rolling_window.max()
-                    df.loc[i,f'min_rolling_{var}'] = rolling_window.min()
-                    df.loc[i,f'perc_25_rolling_{var}'] = rolling_window.quantile(0.25)
-                    df.loc[i,f'perc_75_rolling_{var}'] = rolling_window.quantile(0.75)
+                    df.loc[i,f'median_rolling_{var}'] = rolling_window.median()[i]
+                    df.loc[i,f'dev_std_rolling_{var}'] = rolling_window.std()[i]
+                    df.loc[i,f'mean_rolling_{var}'] = rolling_window.mean()[i]
+                    df.loc[i,f'max_rolling_{var}'] = rolling_window.max()[i]
+                    df.loc[i,f'min_rolling_{var}'] = rolling_window.min()[i]
+                    df.loc[i,f'perc_25_rolling_{var}'] = rolling_window.quantile(0.25)[i]
+                    df.loc[i,f'perc_75_rolling_{var}'] = rolling_window.quantile(0.75)[i]
                     
                     seasonal_window = df[var].rolling(window = M_period, min_periods=M_period)
                     df.loc[i,f'min_seasonal_{var}'] = seasonal_window.min()
@@ -278,15 +295,16 @@ class Preprocessing():
                     var = self.target + "_lag_1"
 
                     rolling_window = df[var].rolling(f'{h_window-1}H', min_periods=h_window-1)
-                    df.loc[f'median_rolling_{var}'] = rolling_window.median()
-                    df.loc[f'dev_std_rolling_{var}'] = rolling_window.std()
-                    df.loc[f'mean_rolling_{var}'] = rolling_window.mean()
-                    df.loc[f'max_rolling_{var}'] = rolling_window.max()
-                    df.loc[f'min_rolling_{var}'] = rolling_window.min()
-                    df.loc[f'perc_25_rolling_{var}'] = rolling_window.quantile(0.25)
-                    df.loc[f'perc_75_rolling_{var}'] = rolling_window.quantile(0.75)
+                    df.loc[i,f'median_rolling_{var}'] = rolling_window.median()
+                    df.loc[i,f'dev_std_rolling_{var}'] = rolling_window.std()
+                    df.loc[i,f'mean_rolling_{var}'] = rolling_window.mean()
+                    df.loc[i,f'max_rolling_{var}'] = rolling_window.max()
+                    df.loc[i,f'min_rolling_{var}'] = rolling_window.min()
+                    df.loc[i,f'perc_25_rolling_{var}'] = rolling_window.quantile(0.25)
+                    df.loc[i,f'perc_75_rolling_{var}'] = rolling_window.quantile(0.75)
                     
                     seasonal_window = df[var].rolling(window = M_period, min_periods = M_period)
+                
                     df.loc[i,f'min_seasonal_{var}'] = seasonal_window.min()
                     df.loc[i,f'max_seasonal_{var}'] = seasonal_window.max()
                     df.loc[i,f'mean_seasonal_{var}'] = seasonal_window.mean()
@@ -317,146 +335,6 @@ class Preprocessing():
 
 
 
-# class ForecastingModelTrainer:
-#     """
-#     Classe per gestire l'allenamento di modelli di forecasting, supportando MLP e XGBoost,
-#     e per eseguire Grid Search per trovare il miglior set di iperparametri.
-#     """
-
-#     def split_data_temporally(self, df: pd.DataFrame, target_column: str, split_date: str = None, train_ratio: float = None):
-#         ################################# QUI #############################
-#         """
-#         Divide il DataFrame in set di allenamento e test basandosi su una divisione temporale.
-
-#         :param df: DataFrame completo.
-#         :param target_column: Nome della colonna target.
-#         :param split_date: Data specifica per dividere i dati (formato 'YYYY-MM-DD').
-#         :param train_ratio: Percentuale di dati da usare per l'allenamento (es. 0.8 per 80%).
-#         :return: Tuple (X_train, y_train, X_test, y_test).
-#         """
-#         if split_date:
-#             train_df = df[df.index < split_date]
-#             test_df = df[df.index >= split_date]
-
-#         elif train_ratio:
-#             split_idx = int(len(df) * train_ratio)
-#             train_df = df.iloc[:split_idx]
-#             test_df = df.iloc[split_idx:]
-
-#         else:
-#             raise ValueError("È necessario fornire una split_date o un train_ratio.")
-
-#         X_train = train_df.drop(target_column, axis=1)
-#         y_train = train_df[target_column]
-#         X_test = test_df.drop(target_column, axis=1)
-#         y_test = test_df[target_column]
-
-#         return X_train, y_train, X_test, y_test
-
-#     # def _initialize_model(self):
-#     #     """
-#     #     Inizializza il modello in base al tipo specificato.
-
-#     #     :return: Il modello inizializzato.
-#     #     """
-#     #     if self.model_type == 'mlp':
-#     #         return MLPRegressor()
-#     #     elif self.model_type == 'xgboost':
-#     #         return XGBRegressor()
-#     #     else:
-#     #         raise ValueError("Tipo di modello non supportato. Scegliere tra 'mlp' o 'xgboost'.")
-
-#     # def fit(self, X_train: pd.DataFrame, y_train: pd.Series):
-
-#     #     """
-#     #     Allena il modello sui dati forniti.
-
-#     #     :param X_train: DataFrame contenente le feature di allenamento.
-#     #     :param y_train: Serie contenente il target di allenamento.
-#     #     """
-#     #     self.model.fit(X_train, y_train)
-
-#     def fit_grid(self, models_params: list, X_train: pd.DataFrame, y_train: pd.Series, cv=5):
-
-#         """
-#         Esegue Grid Search su più modelli per trovare il miglior set di iperparametri.
-
-#         :param models_params: Lista di tuple, ciascuna contenente un modello e la sua griglia di parametri.
-#         :param X_train: DataFrame contenente le feature di allenamento.
-#         :param y_train: Serie contenente il target di allenamento.
-#         :param cv: Numero di fold per la cross-validation.
-#         :return: Il miglior modello e i suoi parametri.
-#         """
-#         best_score = float('-inf')
-#         for model, params in models_params:
-
-#             grid_search = GridSearchCV(model, params, cv=cv, n_jobs=-1, verbose=2)
-#             grid_search.fit(X_train, y_train)
-
-#             if grid_search.best_score_ > best_score:
-
-#                 best_score = grid_search.best_score_
-#                 self.best_model = grid_search.best_estimator_
-#                 self.best_params = grid_search.best_params_
-
-#         self.model = self.best_model
-
-#         return self.model, self.best_params
-
-#     def inner_predict(self, X_test: pd.DataFrame) -> pd.Series:
-
-#         """
-#         Un passo della predizione ricorsiva ( "Predizione ad un passo")
-#         predico il valore target al tempo t in funzione di features relative istanti precedenti fino al tempo t
-
-#         :param X_test: DataFrame contenente le feature necessarie al modello.
-#         """
-#         return self.model.predict(X_test)
-    
-#     def evaluate_model(self, X_test: pd.DataFrame, y_test: pd.Series) -> dict:
-
-#         """
-#         Valuta il modello sui dati di test.
-
-#         :param X_test: DataFrame contenente le feature di test.
-#         :param y_test: Serie contenente il target di test.
-#         :return: Dizionario con le metriche di valutazione.
-#         """
-#         predictions = self.model.predict(X_test)
-#         mse = mean_squared_error(y_test, predictions)
-#         mae = mean_absolute_error(y_test, predictions)
-#         r2  = r2_score(y_test, predictions)
-
-#         return {'MSE': mse, 'MAE': mae, 'R2': r2}
-
-#     # def evaluate_forecasting_model(self, X: pd.DataFrame, y: pd.Series, start_window: str , horizon_window: int):
-
-#     #     for var in target_stats:
-#     #         df.loc[i, f'median_rolling_{var}_lag_1'] = df.loc[(i - pd.Timedelta(hours=23)):i, var + '_lag_1'].median()
-#     #         df.loc[i, f'dev_std_rolling_{var}_lag_1'] = df.loc[i - pd.Timedelta(hours=23):i, var + '_lag_1'].std()
-#     #         df.loc[i, f'media_rolling_{var}_lag_1'] = df.loc[i - pd.Timedelta(hours=23):i, var + '_lag_1'].mean()
-
-
-        
-
-
-#     def save_model(self, path: str):
-#         """
-#         Salva il modello in un file.
-
-#         :param path: Percorso del file in cui salvare il modello.
-#         """
-#         joblib.dump(self.model, path)
-
-#     def load_model(self, path: str):
-#         """
-#         Carica un modello da un file.
-
-#         :param path: Percorso del file da cui caricare il modello.
-#         """
-#         self.model = joblib.load(path)
-
-
 
 ## MAIN ##
 
@@ -469,7 +347,7 @@ df.Datetime = pd.to_datetime(df.Datetime)
 df.set_index('Datetime', inplace = True)
 df.drop(columns = 'Unnamed: 0', inplace = True)
 prep = Preprocessing(df,dict_var_lag)
-df_prep = prep.preprocess('T2M')
+df_prep = prep.preprocess_tlr('T2M')
 # time_shifts = prep.get_time_shifts()
 # # max(time_shifts)
 # # df_prep = df
@@ -483,9 +361,22 @@ model_params = [(MLPRegressor(), {
                                 'batch_size': [32, ]
                                 }
 )]
-x = 1
-(0.53*0.00369)*0.914*(x) + 0.935*(1-x)
-X_train, y_train, X_test, y_test = prep.split_data_temporally(df_prep, prep.target, '2023-10-30')
-model, best_params = prep.fit_grid(model_params, X_train, y_train)
 
+X_train, y_train, X_test, y_test = prep.split_data_temporally(df_prep, prep.target, '2023-10-30')
+model, best_params = prep.fit_grid(model_params, X_train, y_train, 2)
+
+path_test = r'C:\Users\Mipper\Desktop\Progetti\NIA-MED\tlr_class_forecasting\Meteo_1Gen_Dic2023_test_predict.csv'
+df_test = pd.read_csv(path_test, sep = ';')
+df_test.Datetime = pd.to_datetime(df_test.Datetime, format = '%d/%m/%Y %H:%M')
+df_test.set_index('Datetime', inplace = True)
+prep_test = Preprocessing(df_test, prep.dict_features)
+df_test = prep_test.preprocess_tlr(prep.target, is_train = False)
+
+y_pred = prep.predict(df_test,'2023-12-31 00:00',24)
+
+
+import pickle
+path_save = r'C:\Users\Mipper\Desktop\Progetti\NIA-MED\tlr_class_forecasting\model_test.pkl'
+with open(path_save, 'wb') as tf:
+    pickle.dump(model, tf)
 # FINO a qui funziona, testare il resto!
